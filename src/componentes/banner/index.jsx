89d707d92f3a8e8f6af8179e697b5ad7664f029b
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-06 20:27:26 -0300

    pasta Componentes renomeada para componentes

---
+++ src/componentes/banner/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './banner.module.scss'
+import banner from './banner.png'
+
+export default function Banner() {
+  return (
+    <div className={styles.banner}>
+        <h1>A galeria mais completa do espaço</h1>
+        <img src={banner} alt='imagem da terra vista do espaço'/>
+    </div>
+  )
+}
