ee26346f9c6838ce68ae304a0c3ffca07ba7f4f4
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-06 19:45:51 -0300

    Menu da pagina inicial e banner desenvolvidos

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,22 @@
 import Cabecalho from '../../Componentes/Cabecalho'
+import Menu from '../../Componentes/Menu/index.jsx'
+import banner from './banner.png'
+import styles from './PaginaInicial.module.scss'
 
 export default function PaginaInicial () {
     return(
-        <Cabecalho/>
+        <>
+            <Cabecalho />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt='imagem da terra vista do espaço'/>
+                    </div>
+                                        
+                </section>
+            </main>
+        </>
     )
 }
