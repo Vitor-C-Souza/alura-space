f81456ebb9d55de840afeab5123f58bac0ae9592
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-09 02:59:37 -0300

    adicionando absolute imports para deixar o código mais legivel

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,10 +1,11 @@
-import Banner from "../../componentes/banner/index.jsx";
-import Cabecalho from "../../componentes/Cabecalho/index.jsx";
-import Galeria from "../../componentes/Galeria/index.jsx";
-import Menu from "../../componentes/Menu/index.jsx";
-import Populares from "../../componentes/Populares/index.jsx";
-import Rodape from "../../componentes/rodape/index.jsx";
 import styles from "./PaginaInicial.module.scss";
+
+import Banner from "componentes/banner/index.jsx";
+import Cabecalho from "componentes/Cabecalho/index.jsx";
+import Galeria from "componentes/Galeria/index.jsx";
+import Menu from "componentes/Menu/index.jsx";
+import Populares from "componentes/Populares/index.jsx";
+import Rodape from "componentes/rodape/index.jsx";
 
 export default function PaginaInicial() {
   return (
