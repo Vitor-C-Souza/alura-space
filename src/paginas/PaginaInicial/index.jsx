4d81f5978c625ab626fd207759eebbca97048ee4
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-08 22:49:21 -0300

    foi adicionado a Galeria na pagina inicial do site e seus estilos

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,5 +1,6 @@
 import Banner from '../../componentes/banner/index.jsx'
 import Cabecalho from '../../componentes/Cabecalho/index.jsx'
+import Galeria from '../../componentes/Galeria/index.jsx'
 import Menu from '../../componentes/Menu/index.jsx'
 import Rodape from '../../componentes/rodape/index.jsx'
 import styles from './PaginaInicial.module.scss'
@@ -14,6 +15,9 @@ export default function PaginaInicial () {
                     <Banner />                                                            
                 </section>
             </main>
+            <div className={styles.galeria}>
+                <Galeria />
+            </div>
             <Rodape />
         </>
     )
