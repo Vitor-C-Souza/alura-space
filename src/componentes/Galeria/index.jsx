a5b935043e1120b209e7d4bc0d48b0530823383d
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-09 03:00:53 -0300

    Criado a função filtro no site

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tags from '../Tags/index.jsx'
 import styles from './Galeria.module.scss'
 import fotos from './fotos.json'
@@ -6,11 +6,22 @@ import fotos from './fotos.json'
 import Cards from './cards/index.jsx'
 
 export default function Galeria() {
+  const [itens, setItens] = useState(fotos)
+  const tags = [...new Set(fotos.map((valor) => valor.tag))] 
+
+  const filtraFotos = (tag) => {
+    const novasFotos = fotos.filter((foto) => {
+      return foto.tag === tag;
+    })
+
+    setItens(novasFotos)
+  }
+
   return (
     <section className={styles.galeria}>
         <h2>Navegue pela galeria</h2>
-        <Tags />
-        <Cards itens={fotos} styles={styles}/>
+        <Tags tags={tags} filtraFotos={filtraFotos} setItens={setItens} />
+        <Cards itens={itens} styles={styles}/>
     </section>
   )
 }
