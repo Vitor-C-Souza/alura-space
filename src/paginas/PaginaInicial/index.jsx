cbec9c6c1d728fe7c95c130e477c9bc0c5f6f1b8
Vitor-C-Souza <vitor_souza97@hotmail.com>
2023-04-08 23:16:34 -0300

    adicionado a seção ao site

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,24 +1,27 @@
-import Banner from '../../componentes/banner/index.jsx'
-import Cabecalho from '../../componentes/Cabecalho/index.jsx'
-import Galeria from '../../componentes/Galeria/index.jsx'
-import Menu from '../../componentes/Menu/index.jsx'
-import Rodape from '../../componentes/rodape/index.jsx'
-import styles from './PaginaInicial.module.scss'
+import Banner from "../../componentes/banner/index.jsx";
+import Cabecalho from "../../componentes/Cabecalho/index.jsx";
+import Galeria from "../../componentes/Galeria/index.jsx";
+import Menu from "../../componentes/Menu/index.jsx";
+import Populares from "../../componentes/Populares/index.jsx";
+import Rodape from "../../componentes/rodape/index.jsx";
+import styles from "./PaginaInicial.module.scss";
 
-export default function PaginaInicial () {
-    return(
-        <>
-            <Cabecalho />
-            <main>
-                <section className={styles.principal}>
-                    <Menu />  
-                    <Banner />                                                            
-                </section>
-            </main>
-            <div className={styles.galeria}>
-                <Galeria />
-            </div>
-            <Rodape />
-        </>
-    )
+export default function PaginaInicial() {
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <Banner />
+        </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
+      </main>
+
+      <Rodape />
+    </>
+  );
 }
